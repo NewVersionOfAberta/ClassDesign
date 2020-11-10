93cb21081d336d207a7cded18b33836db9a0294b
NewVersionOfAberta <natalia101800@gmail.com>
2020-11-10 15:49:45 +0300

    add: validation of name and surname

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -4,11 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class User {
+    public static final int MAX_NAME_LENGTH = 60;
     private String name;
     private String surname;
     private final int id;
 
     public User(String name, String surname, int id) {
+        if (name == null || surname == null){
+            log.error("Name and surname must not be null");
+            throw new NullPointerException();
+        }
+        if (name.isEmpty() || name.length() > MAX_NAME_LENGTH || surname.isEmpty() || surname.length() < MAX_NAME_LENGTH){
+            log.error("Name and surname must have less then {} and greater then {} characters", MAX_NAME_LENGTH, 0);
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.surname = surname;
         this.id = id;
